f550a25a57c450d1b751fd5a908372edc843a62c
Ian Agustin <inbagustin@gmail.com>
2018-09-21 21:18:54 +0800

    Functional sidebar for concept

--- burger.js
+++ burger.js
@@ -8,7 +8,11 @@ function toggleSidebarNav () {
 }
 
 burgerButton.addEventListener('click', toggleSidebarNav);
-
+window.addEventListener('resize', function(e) {
+  if (window.clientWidth > 752) {
+    nav.style.display = 'block';
+  }
+});
 // On every change of max width, look if the max width
 //is 758px or below then hide the nav bar and put
 // in the burger button with onclick triggers
