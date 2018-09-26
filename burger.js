d211fd308a53aae0579e8d8b9e37d8b6c5fe4951
Ian Agustin <inbagustin@gmail.com>
2018-09-26 19:51:27 +0800

    Sidebar finished

--- burger.js
+++ burger.js
@@ -2,9 +2,12 @@
 
 let nav = document.querySelector('.header-nav-container');
 let burgerButton = document.querySelector('.burger');
+let headerHeight = document.querySelector('.l-header').offsetHeight;
 
 function toggleSidebarNav () {
   nav.classList.toggle('nav-is-shown');
+  nav.style.top = headerHeight + 'px';
+  nav.style.left = '0';
 }
 
 burgerButton.addEventListener('click', toggleSidebarNav);
